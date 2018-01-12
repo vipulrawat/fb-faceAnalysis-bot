f7ed02018af21c86279f19dc56196f5063adfcd1
vipulrawat <vipulrawat.imi@live.in>
2018-01-12 23:08:37 +0530

    Testing

--- microservices/bot/src/server.js
+++ microservices/bot/src/server.js
@@ -121,10 +121,10 @@ function describesImage(result){
     //let cat = JSON.stringify(result.categories[0].detail.celebrities[0].name);
     //let cat = JSON.stringify(result.description.captions);
     //return cat;
-    result.categories[0].detail.celebrities.forEach(element){
+    Object.keys(result.categories[0].detail.celebrities).forEach(function(element){
       let cat = JSON.stringify(element.name);
       return cat
-    }
+    });
   }
   return `no`;
 }
